cf66c29c25be83273dc7d9aba21dde6a75b25c95
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2021-12-31 18:04:49 +0800

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -2,6 +2,7 @@ from machine import Pin,PWM
 import time
 import uasyncio as asyncio
 
+#The dictionary contains 37 notes and their corresponding PWM frequencies
 Tone_Dict={"L1":131,"L1#":139,"L2":147,"L2#":156,"L3":165,"L4":175,"L4#":185,"L5":196,"L5#":208,"L6":220,"L6#":233,"L7":247,
             "1":262, "1#":277, "2":294, "2#":311, "3":330, "4":349, "4#":370, "5":392, "5#":415, "6":440, "6#":466, "7":494,
             "H1":523,"H1#":554,"H2":587,"H2#":622,"H3":659,"H4":698,"H4#":740,"H5":784,"H5#":831,"H6":880,"H6#":932,"H7":988,
