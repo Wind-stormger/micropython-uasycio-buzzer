d54ae4d73e013c5a76438dc967097e69cd7b7d8a
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2022-01-04 11:39:39 +0800

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,26 +1,27 @@
 '''
-The method of editing musical notation in this program is based on the numbered notation described.
-For details, please refer to readme.md
+The method of editing musical notation in this program is based on numbered notation described.
+For details, please refer to README.
 https://github.com/Wind-stormger/micropython-uasycio-buzzer
 '''
 from machine import Pin,PWM
 import time
 import uasyncio as asyncio
 
-#The dictionary contains 37 notes and their corresponding PWM frequencies.
+# The dictionary contains 37 notes and their corresponding PWM frequencies.
 Tone_Dict={"L1":131,"L1#":139,"L2":147,"L2#":156,"L3":165,"L4":175,"L4#":185,"L5":196,"L5#":208,"L6":220,"L6#":233,"L7":247,
             "1":262, "1#":277, "2":294, "2#":311, "3":330, "4":349, "4#":370, "5":392, "5#":415, "6":440, "6#":466, "7":494,
            "H1":523,"H1#":554,"H2":587,"H2#":622,"H3":659,"H4":698,"H4#":740,"H5":784,"H5#":831,"H6":880,"H6#":932,"H7":988,
             "0":0}
 
 class Buzzer:
+    # Initialize a PWM channel
     def __init__(self,pwm=None):
         self.pwm=pwm
         if self.pwm==None:
             self.pwm=PWM(Pin(0,Pin.OUT))
         self.pwm.duty(0)
 
-
+    # Ordinary method of playing music score
     def play(self,score,tempo=60,freq_multiple=1,output=0):
         One_Beat_Time=1000*60/tempo
         if output==1:
@@ -41,6 +42,7 @@ class Buzzer:
             time.sleep_ms(1)
         self.pwm.duty(0)
 
+    # Async method of playing music score
     async def async_play(self,score,tempo=60,freq_multiple=1,output=0,channel=0):
         One_Beat_Time=1000*60/tempo
         if output==1:
