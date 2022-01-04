72e7a5ba5fd5d3fed599b8f03f89788932870dbb
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2022-01-04 11:24:25 +0800

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,19 +1,7 @@
 '''
 The method of editing musical notation in this program is based on the numbered notation described.
-Numbers 1 to 7 represent the musical notes (more accurately the scale degrees). 
-They always correspond to the diatonic major scale.
-For example, in the key of C, their relationship with the notes and the solfège is as follows:
-
-Note:   	C	D	E	F	G	A	B
-Solfège:	do	re	mi	fa	so	la	ti
-Notation:	1	2	3	4	5	6	7
-
-In the numbered notation described,dots above or below a musical note raise or lower it to other octaves. 
-The number of dots equals the number of octaves.
-Here,the dots above a musical note change to "L",such as "L1".
-The dots below a musical note change to "H",such as "H1".
-If it is sharp (higher in pitch by one semitone (half step)), its notation is "#", such as "1#". 
-The number "0" represents the musical rest.
+For details, please refer to readme.md
+https://github.com/Wind-stormger/micropython-uasycio-buzzer
 '''
 from machine import Pin,PWM
 import time
