ecebb057c6d58179163fae20835c7a6900d31cde
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2022-01-04 10:41:23 +0800

    Update buzzer.py

--- buzzer.py
+++ buzzer.py
@@ -1,12 +1,29 @@
+'''
+The method of editing musical notation in this program is based on the numbered notation described.
+Numbers 1 to 7 represent the musical notes (more accurately the scale degrees). 
+They always correspond to the diatonic major scale.
+For example, in the key of C, their relationship with the notes and the solfège is as follows:
+
+Note:   	C	D	E	F	G	A	B
+Solfège:	do	re	mi	fa	so	la	ti
+Notation:	1	2	3	4	5	6	7
+
+In the numbered notation described,dots above or below a musical note raise or lower it to other octaves. 
+The number of dots equals the number of octaves.
+Here,the dots above a musical note change to "L",such as "L1".
+The dots below a musical note change to "H",such as "H1".
+If it is sharp (higher in pitch by one semitone (half step)), its notation is "#", such as "1#". 
+The number "0" represents the musical rest.
+'''
 from machine import Pin,PWM
 import time
 import uasyncio as asyncio
 
-#The dictionary contains 37 notes and their corresponding PWM frequencies
+#The dictionary contains 37 notes and their corresponding PWM frequencies.
 Tone_Dict={"L1":131,"L1#":139,"L2":147,"L2#":156,"L3":165,"L4":175,"L4#":185,"L5":196,"L5#":208,"L6":220,"L6#":233,"L7":247,
             "1":262, "1#":277, "2":294, "2#":311, "3":330, "4":349, "4#":370, "5":392, "5#":415, "6":440, "6#":466, "7":494,
-            "H1":523,"H1#":554,"H2":587,"H2#":622,"H3":659,"H4":698,"H4#":740,"H5":784,"H5#":831,"H6":880,"H6#":932,"H7":988,
-            "-":1}
+           "H1":523,"H1#":554,"H2":587,"H2#":622,"H3":659,"H4":698,"H4#":740,"H5":784,"H5#":831,"H6":880,"H6#":932,"H7":988,
+            "0":0}
 
 class Buzzer:
     def __init__(self,pwm=None):
@@ -25,8 +42,12 @@ class Buzzer:
             play_time=round(beat * One_Beat_Time)
             if output==1:
                 print("Beat:%s,Tone:%s,Freq:%s" % (beat,tone1,play_freq))
-            self.pwm.freq(play_freq)
-            self.pwm.duty(512)
+            if play_freq==0:
+                self.pwm.duty(0)
+            else:
+                if not self.pwm.freq() == play_freq:
+                    self.pwm.freq(play_freq)
+                self.pwm.duty(512)
             time.sleep_ms(play_time)
             self.pwm.duty(0)
             time.sleep_ms(1)
@@ -41,11 +62,14 @@ class Buzzer:
             play_time=round(beat * One_Beat_Time)
             if output==1:
                 print("Channel:%s,Beat:%s,Tone:%s,Freq:%s" % (channel,beat,tone1,play_freq))
-            for i in range(0,play_time+1,1):
-                if not self.pwm.freq() == play_freq:
-                    self.pwm.freq(play_freq)
-                self.pwm.duty(512)
-                await asyncio.sleep_ms(1)
+            for i in range(0,play_time+10,10):
+                if play_freq==0:
+                    self.pwm.duty(0)
+                else:
+                    if not self.pwm.freq() == play_freq:
+                        self.pwm.freq(play_freq)
+                    self.pwm.duty(512)
+                await asyncio.sleep_ms(10)
             self.pwm.duty(0)
             await asyncio.sleep_ms(1)
         self.pwm.duty(0)
