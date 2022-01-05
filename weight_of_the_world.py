8128a64f3ec50c9690d676c284bb332e61cbf31b
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2022-01-05 12:00:46 +0800

    Create weight_of_the_world.py

---
+++ weight_of_the_world.py
@@ -0,0 +1,32 @@
+'''
+Licence
+The music score used in this code comes from https://www.qupu123.com/puyou/jipu/p289352.html
+Comply with the CC BY-NC-SA 3.0 license
+'''
+from machine import Pin,PWM
+import time
+import buzzer
+import uasyncio as asyncio
+
+Weight_of_the_World=[
+    (1/2,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"2"),(1/2,"5"),
+    (1/4,"L6"),(1/4,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"5"),
+    (1/1,"5"),
+    
+    (1/2,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"2"),(1/2,"6"),
+    (1/4,"2"),(1/4,"2"),(1/4,"3"),(1/4,"2"),(1/4,"6"),
+    (1/1,"6"),
+    
+    (1/2,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"2"),(1/2,"5"),
+    (1/4,"L6"),(1/4,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"5"),
+    (1/1,"5"),
+    
+    (1/2,"L6"),(1/4,"3"),(1/4,"2"),(1/4,"2"),(1/2,"6"),
+    (1/4,"2"),(1/4,"2"),(1/4,"3"),(1/4,"2"),(1/4,"6"),
+    (1/1,"6"),
+    ]
+
+buzzer=buzzer.Buzzer(PWM(Pin(1,Pin.OUT)))
+buzzer.play(Weight_of_the_World,tempo=30,freq_multiple=1,output=0)
+
+
